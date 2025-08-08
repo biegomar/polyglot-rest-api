a2db974a5c61004ab3b6f1c9bfde111fdfa91dfb
biegomar <marc.biegota@web.de>
2025-08-08 23:30:42 +0200

    feature: add some more meta data

---
+++ spring-boot/UserManagement/src/main/java/de/biegota/usermanagement/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,31 @@
+package de.biegota.usermanagement.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development");
+
+        Contact myContact = new Contact();
+        myContact.setName("biegomar");
+        myContact.setUrl("https://github.com/biegomar");
+
+        Info information = new Info()
+                .title("User Management System API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage users of the system.")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
